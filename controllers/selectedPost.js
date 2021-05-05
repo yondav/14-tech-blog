c8d37d35ab8506f7894b7ed6be3265c99e04acd9
yondav <yonidavid.abr@gmail.com>
2021-05-05 14:16:21 -0400

    don't understand why the link to single post is bringing me back to the homepage

--- controllers/selectedPost.js
+++ controllers/selectedPost.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // Get post by id, include author, comments and comment_authors
 // For single post view
-router.get('/:id', async (req, res) => {
+router.get('/:id', withAuth, async (req, res) => {
   try {
     const newPostData = await Post.findByPk(req.params.id, {
       include: [
@@ -23,8 +24,9 @@ router.get('/:id', async (req, res) => {
         },
       ],
     });
-    const postData = postData.get({ plain: true });
-    console.log(postData);
+    const post = newPostData.get({ plain: true });
+    res.render('signup', post);
+    console.log('console.log', post);
     res.status(200).json(newPostData);
   } catch (err) {
     res.status(400).json('Page not found!');
