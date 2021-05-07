13380be580a02fbc36304968727ad0990bb5e155
yondav <yonidavid.abr@gmail.com>
2021-05-07 11:52:26 -0400

    comment edit works

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,9 +2,10 @@ const post = document.querySelector('.post');
 const newComment = document.querySelector('.new-comment');
 const commentBody = document.querySelector('.comment-body');
 const commentDelete = document.querySelectorAll('.comment-delete');
+const commentEdit = document.querySelectorAll('.comment-edit');
+const userId = commentBody.dataset.user;
 
 const commentFormHandler = async (event) => {
-  const userId = commentBody.dataset.user;
   event.preventDefault();
   const postId = post.dataset.id;
   const commentContent = commentBody.value;
@@ -45,4 +46,32 @@ commentDelete.forEach((comment) => {
 
   comment.addEventListener('click', commentDeleteHandler);
 });
-// commentDelete.addEventListener('click', commentDeleteHandler);
+
+commentEdit.forEach((comment) => {
+  commentEditHandler = () => {
+    event.preventDefault;
+    const commentId = comment.dataset.comment;
+    const editForm = document.querySelectorAll('.edit-comment');
+
+    editForm.forEach((form) => {
+      form.classList.remove('hide');
+      form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const commentContent = commentBody.value;
+
+        const response = await fetch(`/api/comments/${commentId}`, {
+          method: 'PUT',
+          body: JSON.stringify({ user_id: userId, comment_content: commentContent }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+          document.location.reload();
+        } else {
+          alert('Failed to edit comment.');
+        }
+        console.log(response);
+      });
+    });
+  };
+  comment.addEventListener('click', commentEditHandler);
+});
